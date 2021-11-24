54a9ec49d56d6a6e1628d925a7a3572cae040f0b
kakachake <282281884@qq.com>
2021-11-24 17:42:40 +0800

    update readme

--- dist/util.d.ts
+++ dist/util.d.ts
@@ -22,3 +22,20 @@ export declare function getMapTypeAndValue(val: string): {
  * @return 返回查找到的元素
  */
 export declare function getEl(selector: string, context?: Document): HTMLElement | HTMLCollectionOf<Element> | null;
+export declare class Throttle {
+    /**
+     *
+     * @param fn
+     * @param awit
+     */
+    static use(fn: Function, awit?: number): (...args: any) => void;
+}
+export declare class Debounced {
+    /**
+     *
+     * @param fn 要执行的函数
+     * @param awit  时间
+     * @param immediate 是否在触发事件后 在时间段n开始，立即执行，否则是时间段n结束，才执行
+     */
+    static use(fn: Function, awit?: number, immediate?: boolean): (...args: any) => void;
+}
