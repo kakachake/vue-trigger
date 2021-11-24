c398bf3e70b6e1e03a5fd2480b3dde9489e5015c
kakachake <282281884@qq.com>
2021-11-24 17:46:35 +0800

    update readme

--- example/src/main.js
+++ example/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import trigger from './trigger/index'
+import trigger from 'vue-trigger'
 
 createApp(App).use(router).use(trigger).mount('#app')
